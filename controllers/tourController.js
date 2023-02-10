5f7ec129c04c824a3e159b78048d6d7d2e8a3c1e
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-02-10 15:18:15 +0530

    Implementing CRUD Operations

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,34 +4,47 @@ const Tour = require("../models/tourModel");
 // ******************************************** Handler Functions ********************************************* 
 
 // Function handling the get() request of all the tours.
-exports.getAllTours = (req, res) => {
-    console.log(req.requestTime);
+exports.getAllTours = async (req, res) => {
 
-    // We send the data in the Jsend data format of the JSON.
-    res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        // results: tours.length,
-        // data: {
-        //     tours
-        // }
-    });
+    try{
+        const tours = await Tour.find();
+    
+        res.status(200).json({
+            status: 'success',
+            results: tours.length,
+            data: {
+                tours
+            }
+        });
+    }
+    catch(err){
+        res.status(404).json({
+            status: "fail",
+            message: err
+        });
+    }
 };
 
 // Function handling the get() request to get a specific tour from all the tours.
-exports.getOneTour = (req, res) => {
-    // To read the parameters
-    console.log(req.params);
+exports.getOneTour = async (req, res) => {
 
-    // const id = req.params.id * 1;
-    // const tour = tours.find(el => el.id === id);  // For finding the tour with the given id.
-            
-    res.status(200).json({
-        status: 'success',
-        // data: {
-        //     tour
-        // }
-    });
+    try{
+        const tour = await Tour.findById(req.params.id);
+        // Tour.findById(req.params.id) == Tour.findOne({ _id: req.params.id }) (In MongoDB)
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        });
+    }
+    catch(err){
+        res.status(404).json({
+            status: "fail",
+            message: err
+        });
+    }
 };
 
 // Function handling the post() request to create a new tour in the tours collection.
@@ -61,21 +74,46 @@ exports.createTour = async (req, res) => {
 
 
 // Function handling the update() request to update a specific tour in the tours data.
-exports.updateTour = (req, res) => {
-    res.status(200).json({
-        status: "success",
-        data: {
-            tour: '<Updated tour here...>'
-        }
-    });
+exports.updateTour = async (req, res) => {
+
+    try{
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+
+        res.status(200).json({
+            status: "success",
+            data: {
+                tour
+            }
+        });
+    }
+    catch(err){
+        res.status(404).json({
+            status: "fail",
+            message: err
+        });
+    }
 };
 
 // Function handling the delete() request to delete a specific tour from the tours data.
-exports.deleteTour = (req, res) => {
-    res.status(204).json({
-        status: "success",
-        data: null
-    });
+exports.deleteTour = async (req, res) => {
+
+    try{
+        await Tour.findByIdAndDelete(req.params.id);
+
+        res.status(204).json({
+            status: "success",
+            data: null
+        });
+    }
+    catch(err){
+        res.status(404).json({
+            status: "fail",
+            message: err
+        });
+    }
 };
 
 // Here all the handler functions are in the exports object.
