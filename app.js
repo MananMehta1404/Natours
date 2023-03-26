b0248cec0d8e10b355c45b04045c613c0e58dc55
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-03-26 15:11:16 +0530

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 // Including a third-party middleware.
 // Using morgan only when we are in development environment.
 // console.log(process.env.NODE_ENV);
-if(process.env.NODE_ENV == 'development'){
+if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
@@ -23,10 +23,10 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 // Creating our own Middleware Function -> The order is very important here as if we create this after the route handlers then it will not be called as the request-responde cycle has already ended. So always try to define it globally on the top of the file.
-app.use((req, res, next) => {
-    console.log('Hello from the middleware...');
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log('Hello from the middleware...');
+//     next();
+// });
 
 // Let's create a new middleware function to manipulate the request object.
 app.use((req, res, next) => {
@@ -39,5 +39,13 @@ app.use((req, res, next) => {
 // Mounting the Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Handling Unhandled Routes
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server!`
+    })
+});
 
 module.exports = app;
