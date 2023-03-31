c1d5121ed2f448eb104a7517ec7e8e2c70d21127
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-03-31 14:48:38 +0530

    Unhandled Rejections

--- server.js
+++ server.js
@@ -47,6 +47,14 @@ mongoose.connect(DB, {
 // ******************************************* Starting the server ********************************************
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log('UNHANDLED REJECTION! Shutting Down...');
+    server.close(() => {
+        process.exit(1);
+    });
 });
