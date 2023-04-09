748e1ba7085d3aa5219b49eb7df14448ed8ba54a
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-04-09 12:24:43 +0530

    Implemented getAllUsers Handler

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,22 @@
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
 // ******************************************** Handler Functions ********************************************* 
 
 // Functions handling the users routes
-exports.getAllUsers = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined!'
+exports.getAllUsers = catchAsync(async (req, res) => {
+    // Executing the query
+    const users = await User.find();
+
+    // Send Response
+    res.status(200).json({
+        status: 'success',
+        results: users.length,
+        data: {
+            users
+        }
     });
-};
+});
 
 exports.getOneUser = (req, res) => {
     res.status(500).json({
