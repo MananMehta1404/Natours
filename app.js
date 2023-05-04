631556eef4ebbea8780e59316e443862acd47ad8
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-05-04 12:27:47 +0530

    Data Sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitze = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -34,6 +36,12 @@ app.use('/api', limiter);
 
 // Including a middle-ware to get the client data available in the request object.
 app.use(express.json({ limit: '10kb' }));
+
+// Data Sanitization against NoSQL query injection.
+app.use(mongoSanitze());
+
+// Data Sanitization against XSS.
+app.use(xss());
 
 // Built-in Express Middleware to serve static files.
 app.use(express.static(`${__dirname}/public`));
