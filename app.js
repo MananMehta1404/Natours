a8bc04bb06bd3cf461e5ac317cbbc6c992922452
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-05-23 16:47:25 +0530

    Extending our Base template with Blocks

--- app.js
+++ app.js
@@ -87,8 +87,19 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
     res.status(200).render('base', {
-        tour: 'The Forest Hiker',
-        user: 'Manan'
+        title: 'Exciting Tours For Adventurous People'
+    });
+});
+
+app.get('/overview', (req, res) => {
+    res.status(200).render('overview', {
+        title: 'All Tours'
+    });
+});
+
+app.get('/tour', (req, res) => {
+    res.status(200).render('tour', {
+        title: 'The Forest Hiker Tour'
     });
 });
 
