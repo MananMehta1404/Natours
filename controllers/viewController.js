90930a2662026708b36fda75b0737a31c9037527
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-06-17 11:53:35 +0530

    Building the Login Screen

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,7 +1,8 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
-exports.getOverview = catchAsync(async (req, res) => {
+// For the getOverview and getTour functions, we need to get the data from the database and then render the template.
+exports.getOverview = catchAsync(async (req, res, next) => {
     // 1) Get tour data from collection
     const tours = await Tour.find();
 
@@ -13,7 +14,7 @@ exports.getOverview = catchAsync(async (req, res) => {
     });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
     // 1) Get the data, for the requested tour (including reviews and guides)
     const tour = await Tour.findOne({ slug: req.params.slug }).populate({
         path: 'reviews',
@@ -27,3 +28,10 @@ exports.getTour = catchAsync(async (req, res) => {
         tour
     });
 });
+
+// For the getLoginForm function, we just need to render the template.
+exports.getLoginForm = (req, res) => {
+    res.status(200).render('login', {
+        title: 'Log into your account'
+    });
+}
